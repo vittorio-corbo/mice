711c47f48f0331eea93d31529a87fe095953539e
Vittorio Corbo <vittocorbo@gmail.com>
2024-05-11 14:12:26 -0400

    add sample sounds and building strcture

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -6,22 +6,35 @@ public class Camera : MonoBehaviour
 {
     // Define the rotation amount
     public float rotationAmount = 45f;
+    public int objSelected = 0;
+
+    [SerializeField] SoundObjectController cube;
 
     // Update is called once per frame
     void Update()
     {
         // Check for left arrow key press
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && (objSelected != -1))
         {
             // Rotate the object to the left by the defined amount
             transform.Rotate(Vector3.up, -rotationAmount);
+
+            objSelected -= 1;
         }
 
         // Check for right arrow key press
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) && (objSelected != 1))
         {
             // Rotate the object to the right by the defined amount
             transform.Rotate(Vector3.up, rotationAmount);
+
+            objSelected += 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        // if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            cube.PlaySound();
         }
     }
 }
---
+++ Assets/SoundObjectController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundObjectController : MonoBehaviour
+{
+    // AudioSource m_MyAudioSource;
+    [SerializeField]  int pizza;//set you up babyyy
+    [SerializeField] AudioSource source;
+
+    //make this into a list
+    [SerializeField] AudioClip[] audioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound(){
+        // source.outputAudioMixerGroup = clip;
+        source.clip = audioClip[0];
+        source.Play();
+        //audio. = otherClip;
+
+    }
+}
